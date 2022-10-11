c21a959618dd1a6d749378a105520f4f49400a3c
Lisa-Maria Bieker <lisa.bieker@web.de>
2022-10-11 16:50:57 +0200

    Finalized iteration9 with some tests remain red

--- src/chronometer-centiseconds.js
+++ src/chronometer-centiseconds.js
@@ -1,37 +1,59 @@
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      if (printTimeCallback) printTimeCallback();
+    }, 10);
   }
 
   getMinutes() {
-    // ... your code goes here
+    let minutes = Math.floor(this.currentTime / 6000);
+    return minutes;
   }
 
   getSeconds() {
-    // ... your code goes here
+    let remainingSeconds = this.currentTime / 100;
+    return remainingSeconds;
   }
 
   getCentiseconds() {
-    // ... your code goes here
+    if (this.currentTime === 0) {
+      return 0;
+    } else {
+      return this.currentTime % 10;
+    }
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    let valueString = value.toString();
+    if (valueString.length === 2) {
+      return valueString;
+    } else if (value === 0) {
+      valueString = "00";
+      return valueString;
+    } else {
+      valueString = "0" + valueString;
+      return valueString;
+    }
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let formattedMinutes = this.computeTwoDigitNumber(this.getMinutes());
+    let formattedSeconds = this.computeTwoDigitNumber(this.getSeconds());
+    let formattedCentiSeconds = this.computeTwoDigitNumber(this.getCentiseconds());
+    return `${formattedMinutes}:${formattedSeconds}.${formattedCentiSeconds}`;
   }
 }
